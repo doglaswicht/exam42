b798f33fad786e3c65120a269efcd42daf3a4b58
doglasbastos <doglasbastos@hotmail.com>
2025-04-12 14:26:58 +0200

    push ft_swap

---
+++ nv0/ft_swap/ft_swap.c
@@ -0,0 +1,22 @@
+
+void	ft_swap(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/*int main(void)
+{
+	int a = 42;
+	int b = 84;
+
+	printf("%d\n", a);
+	printf("%d\n", b);
+
+		ft_swap(&a, &b);
+
+	printf("%d\n", a);
+	printf("%d\n", b);
+}*/
