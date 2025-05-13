a5d8617286cef698766e4f6ebce049ecfbcd0474
Doglas <90707636+doglaswicht@users.noreply.github.com>
2025-05-13 21:19:22 +0200

    Update str_capitalizer.c

--- nv2/str_capitalizer/str_capitalizer.c
+++ nv2/str_capitalizer/str_capitalizer.c
@@ -1,34 +1,26 @@
 #include <unistd.h>
-#include <stdlib.h>
 
-int    is_alpha(char c)
+int is_alpha(char c)
 {
     return((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
 
-void    str_capitalizer(char *str)
+void str_capitalizer(char *str)
 {
     int i = 0;
     int new_word = 1;
-
     while(str[i])
     {
-        if (str[i] >= 'A' && str[i] <= 'Z')
-        {
-            str[i] = str[i] + 32;
-        }
+        if(str[i] >= 'A' && str[i] <= 'Z')
+            str[i] += 32;
         if(is_alpha(str[i]))
         {
             if(new_word && (str[i] >= 'a' && str[i] <= 'z'))
-            {
-                str[i] = str[i] - 32;
-            }
+                str[i] -= 32;
             new_word = 0;
         }
-        else if(str[i] == ' ' || str[i] == '\t') // <-- trocou aqui
-        {
+        else if(str[i] == ' ' || str[i] == '\t')
             new_word = 1;
-        }
         else
             new_word = 0;
         write(1, &str[i], 1);
@@ -38,18 +30,17 @@ void    str_capitalizer(char *str)
 
 int main(int argc, char **argv)
 {
-    if(argc >= 2)
+    if(argc >=2)
     {
         int i = 1;
-        
         while(i < argc)
         {
             str_capitalizer(argv[i]);
             write(1, "\n", 1);
             i++;
-        }    
+        }
     }
     else
         write(1, "\n", 1);
-    return(0);
+    return (0);
 }
